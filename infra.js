e2f348da90e06352664a6be0236b3ac7347300a8
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-01-19 06:45:29 +0400

    fix for uley

--- infra.js
+++ infra.js
@@ -1,4 +1,4 @@
-infra.listen(infrajs,'onshow',function(){
+Event.handler('Infrajs.onshow', function () {
 	//Клик по контролам не должен скролить страницу
 	$('.flexslider .flex-direction-nav a, .flexslider .flex-control-nav a').attr('nohref',1);
 	flexslider();
--- infra.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace itlife\flexslider;
-global $infra;
-infra_when($infra,'onjs', function () {
-	global $infra;
-	$infra['js'] .= $infra['require']('*flexslider/flexslider.js');
-	$infra['js'] .= $infra['require']('*flexslider/infra.js');
-});
