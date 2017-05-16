15acee4f605ec6ab3a8581b3302f2859e49f0b97
Anton Kiyatkin <axlbant@sent.com>
2017-05-16 13:17:02 +0400

    Update infra.js

--- infra.js
+++ infra.js
@@ -1,4 +1,4 @@
-Event.handler('Infrajs.onshow', function () {
+Event.handler('Controller.onshow', function () {
 	//Клик по контролам не должен скролить страницу
 	$('.flexslider .flex-direction-nav a, .flexslider .flex-control-nav a').attr('nohref',1);
 	flexslider();
