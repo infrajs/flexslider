06c6ca2dca1e1626c2f45e3a4cc3ae5b74322fdd
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-02-27 15:39:03 +0400

    char access

--- infra.js
+++ infra.js
@@ -1,5 +1,8 @@
-Event.handler('Controller.onshow', function () {
+Event.handler('Controller.onshow', async () => {
 	//Клик по контролам не должен скролить страницу
+	let Load = (await import('/vendor/akiyatkin/load/Load.js')).default
+	let CDN = await Load.on('import-default', '/vendor/akiyatkin/load/CDN.js')
+	await CDN.load('jquery');
 	$('.flexslider .flex-direction-nav a, .flexslider .flex-control-nav a').attr('nohref',1);
 	flexslider();
 });
